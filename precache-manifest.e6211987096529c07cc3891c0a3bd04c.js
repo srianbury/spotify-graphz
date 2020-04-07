2fed19a674f06f79b19e02f0c318a31f58f384f0
Brian Sunbury (via Travis CI) <bsunbury29@gmail.com>
2020-04-07 16:09:44 +0000

    Deploy srianbury/spotify-graphz to github.com/srianbury/spotify-graphz.git:gh-pages

---
+++ precache-manifest.e6211987096529c07cc3891c0a3bd04c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e0466cffba77a99ca0dc76592e51168",
+    "url": "/spotify-graphz/index.html"
+  },
+  {
+    "revision": "610eaeda7273b1dffe76",
+    "url": "/spotify-graphz/static/css/main.0c38955f.chunk.css"
+  },
+  {
+    "revision": "6d9055c856a3c33cfc18",
+    "url": "/spotify-graphz/static/js/2.9be42adb.chunk.js"
+  },
+  {
+    "revision": "55e11a1652f6f0411636be251f1a75be",
+    "url": "/spotify-graphz/static/js/2.9be42adb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "610eaeda7273b1dffe76",
+    "url": "/spotify-graphz/static/js/main.43a21578.chunk.js"
+  },
+  {
+    "revision": "ee0e5dc296b44f9447b9",
+    "url": "/spotify-graphz/static/js/runtime-main.9d256e6e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.88738d3d7e9016b077cdb16b9fced8ef.js"
+  "/spotify-graphz/precache-manifest.e6211987096529c07cc3891c0a3bd04c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/spotify-graphz/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
